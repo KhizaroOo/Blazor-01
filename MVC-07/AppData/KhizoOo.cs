0eb7f0e9b69d794c4d4a4ba1c4d7e336dbe8c4af
khizooo <khizar.imtiaz@vaival.com>
2024-02-12 11:05:48 +0500

    S

---
+++ MVC-07/AppData/KhizoOo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Blazor_01.AppData
+{
+	public class KhizoOo
+	{
+
+		public KhizoOo() { }
+
+
+        public string ID { get; set; }
+        public string FirstName = "Khizar";
+        public string LastName = "Imtiaz";
+        public string Age = "32 years";
+        public string Freelance = "available";
+
+        public string FullName = "Khizar Imtiaz";
+        public string ShortName = "Khizar";
+        public string NickName = "KhizoOo";
+
+        public string FullBio = "";
+        public string ShortBio = "";
+        public string Birthdate = "1991-09-12";
+        public string AddressOne = "Lahore, Pakistan";
+        public string AddressTwo = "Lahore, Pakistan";
+        public string AddressThree = "Lahore, Pakistan";
+        public string PhoneNumberOne = "+923007683396";
+        public string PhoneNumberTwo = "+923007683396";
+        public string PhoneNumberThree = "+923007683396";
+        public string Email = "khizar.imtiaz@gmail.com";
+        public string Occupation = "Tech-Savvy Artist";
+        public string Nationality = "Pakistani";
+
+        public string IsMarried = "No";
+        public string BloodType = "O+";
+        public string HairColor = "Grey, Golden, Brown";
+        public string EyeColor = "Brown";
+        public string HeightInFoot = "5.7 foot";
+        public string WeightInKg = "90 kg";
+        public string FavoriteColor = "Black";
+        public string FavoriteFood = "Biryani";
+        public string Hobbies = "Drawing, Reading";
+        public string LanguagesSpoken = "Urddu, English";
+        public string ReligiousBeliefs = "Islam";
+        public string PoliticalViews = "Republic";
+
+        public string MySkills = @"[ { 'mt': 'Software Development', 'data': [ { 't': '.NETCore', 'p': 95 }, { 't': 'Blazor', 'p': 90 }, { 't': 'C#', 'p': 98 }, { 't': 'JavaScript', 'p': 97 }, { 't': 'jQuery', 'p': 96 }, { 't': 'React', 'p': 85 }, { 't': 'HTML5', 'p': 80 }, { 't': 'CSS3', 'p': 80 }, { 't': 'Node.js', 'p': 70 }, { 't': 'Express.js', 'p': 70 }, { 't': 'SASS/SCSS', 'p': 80 }, { 't': 'Webpack', 'p': 80 }, { 't': 'TypeScript', 'p': 80 }, { 't': 'Responsive', 'p': 80 }, { 't': 'PWAs', 'p': 90 }, { 't': 'WPO', 'p': 85 }, { 't': 'Web Security', 'p': 90 }, { 't': 'GraphQL', 'p': 80 }, { 't': 'RESTful APIs', 'p': 95 }, { 't': 'AJAX', 'p': 90 }, { 't': 'WebSockets', 'p': 90 }, { 't': 'WebRTC', 'p': 80 }, { 't': 'CMS', 'p': 90 }, { 't': 'eCommerce Platforms', 'p': 90 }, { 't': 'SEO', 'p': 70 }, { 't': 'Web-Storage', 'p': 85 }, { 't': 'SPAs', 'p': 90 }, { 't': 'Code Reviews', 'p': 90 } ] }, { 'mt': 'Blockchain Technologies', 'data': [ { 't': 'Web3.js', 'p': 90 }, { 't': 'Smart Contracts', 'p': 85 }, { 't': 'DApps', 'p': 90 }, { 't': 'Off-Chain', 'p': 90 }, { 't': 'Blockchain in Gaming', 'p': 90 }, { 't': 'Wallet Management', 'p': 85 }, { 't': 'Use Cases', 'p': 90 } ] }, { 'mt': 'Database Management', 'data': [ { 't': 'NoSQL', 'p': 90 }, { 't': 'RDBMS', 'p': 90 }, { 't': 'Design', 'p': 90 }, { 't': 'Optimization', 'p': 90 } ] }, { 'mt': 'Deployment and DevOps', 'data': [ { 't': 'CI/CD', 'p': 80 }, { 't': 'Automation', 'p': 75 }, { 't': 'DevOps Practices', 'p': 85 } ] }, { 'mt': 'Version Control', 'data': [ { 't': 'Git', 'p': 90 }, { 't': 'GitHub', 'p': 90 }, { 't': 'GitLab', 'p': 90 } ] }, { 'mt': 'Project Management', 'data': [ { 't': 'Agile', 'p': 90 }, { 't': 'Scrum', 'p': 90 }, { 't': 'Kanban', 'p': 90 } ] }, { 'mt': 'Programming Paradigms', 'data': [ { 't': 'OOP', 'p': 90 }, { 't': 'Functional', 'p': 90 }, { 't': 'Event-Driven', 'p': 90 }, { 't': 'Data-Driven', 'p': 90 }, { 't': 'Procedural', 'p': 90 } ] }, { 'mt': 'Cloud Technologies', 'data': [ { 't': 'AWS', 'p': 90 }, { 't': 'Azure', 'p': 85 }, { 't': 'Serverless', 'p': 90 }, { 't': 'Microservices', 'p': 90 }, { 't': 'Containers', 'p': 85 }, { 't': 'Docker', 'p': 85 }, { 't': 'Kubernetes', 'p': 85 } ] }, { 'mt': 'Client-Side Technologies', 'data': [ { 't': 'UI', 'p': 85 }, { 't': 'UX', 'p': 90 } ] }";
+        public string MyExperiences = @"[ { 'c': 'Vaival Technology', 'p': 'Project Delivery Manager', 'd': 'Software Developer to Blockchain Product Developer then Technical Team Lead and now Project Delivery Manager at Vulcan Forged.', 't': 'Mar 2017 - Present' }, { 'c': 'Jotana Tech', 'p': '.NET DEVELOPER', 'd': 'Software Developer And My Responsibility Here Is Working On .NET MVC Application Both Desktop And Web.', 't': 'Aug 2015 - Mar 2017' }, { 'c': 'Ikaroobar.com', 'p': 'WEB DEVELOPER', 'd': 'Front-End Developer And My Responsibility To Handle The Product evento.pk Which We Want To Launch In Market.', 't': 'Jan 2015 - Jun 2015' } ]";
+        public string MyEducations = @"[ { 'c': 'Government College University Faisalabad', 'p': 'BSIT', 'd': 'University Degree', 't': '2010 - 2014' }, { 'c': 'Government College Faisalabad', 'p': 'Computer Science', 'd': 'College Degree', 't': '2008 - 2010' } ]";
+
+
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string LinkedIn { get; set; }
+        public string OpenSea { get; set; }
+        public string Skype = "khizar.imtiaz";
+        public string Snapchat { get; set; }
+        public string TikTok { get; set; }
+        public string Twitter { get; set; }
+        public string YouTube { get; set; }
+        public string Pinterest { get; set; }
+
+        #region COMMON METHODS
+
+        #endregion
+
+
+    }
+
+    public class SocialProfile
+    {
+        #region SOCIAL
+
+       
+
+        #endregion
+    }
+
+    public class KhizoOoImages
+    {
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string RouteLink { get; set; }
+    }
+
+
+}
+
---
+++ MVC-07/AppData/Module.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Blazor_01.AppData
+{
+	public class Module
+	{
+        public long ID { get; set; }
+        public string Title { get; set; }
+
+        public Module()
+		{
+		}
+	}
+}
+
