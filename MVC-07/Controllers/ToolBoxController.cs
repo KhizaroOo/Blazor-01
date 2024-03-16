0f0293475112021e8ac8e5dd88f23698cdb6aab5
khizooo <khizar.imtiaz@vaival.com>
2024-03-17 00:07:39 +0500

    A

---
+++ MVC-07/Controllers/ToolBoxController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_07.Controllers
+{
+    public class ToolBoxController : Controller
+    {
+
+        #region Listing
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Color Contrast Checker
+
+        public IActionResult Color_Contrast_Checker()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
+
